e60e7b48d911b3a828b06133310573b94fdd9830
gitingrid <49993985+gitingrid@users.noreply.github.com>
2019-05-11 20:22:37 +0300

    minor updates 11/5/19
    Finally, we solved the errors on the site, drunk beer and f♥ck girl

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 /*------------------------------------------------------------------*/
 /*Прокрутка наверх*/
-$(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() != 0) {
-            $('#up').fadeIn();
-        } else {
-            $('#up').fadeOut();
-        }
-    });
-    $('#up').click(function() {
-        $('body,html').animate({ scrollTop: 0 }, 800);
-    });
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#up').fadeIn(); 
+        } else { 
+            $('#up').fadeOut(); 
+        } 
+    }); 
+    $('#up').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
 });
+
 /*------------------------------------------------------------------*/
 /*Карусель знизу*/
 var swiper = new Swiper('.swiper-container', {
